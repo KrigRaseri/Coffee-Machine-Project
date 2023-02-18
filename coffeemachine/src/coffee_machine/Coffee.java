23ff665e3a2ccaced04ea9e0c4060dea58deed39
Krig Raseri <krigraseri@gmail.com>
2023-02-18 03:44:28 -0600

    Update coffee class
    Adds a totalCups method, updates makeBeanJuice, and a few minor updates.

--- coffeemachine/src/coffee_machine/Coffee.java
+++ coffeemachine/src/coffee_machine/Coffee.java
@@ -3,6 +3,7 @@ package coffee_machine;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Coffee {
     private int coffeeBeans;
@@ -13,18 +14,22 @@ public class Coffee {
     public void makeBeanJuice() {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Write how many cups of coffee you will need:");
-        System.out.println("> ");
+        System.out.print("> ");
 
         try {
             int cups = Integer.parseInt(reader.readLine());
-            setWater(200 * cups); //In ml (milliliters).
-            setMilk(50 * cups); //In ml (milliliters).
-            setCoffeeBeans(15 * cups); //In g (grams).
+            int totalCups = totalCups(getWater(), getMilk(), getCoffeeBeans());
 
-            System.out.printf("For %d cups of coffee you will need:\n", cups);
-            System.out.printf("%d ml of water\n", getWater());
-            System.out.printf("%d ml of milk\n", getMilk());
-            System.out.printf("%d g of coffee beans\n", getCoffeeBeans());
+            if (cups == totalCups) { System.out.println("Yes, I can make that amount of coffee"); }
+
+            else if (cups < totalCups) {
+                System.out.printf("Yes, I can make that amount of coffee " +
+                        "(and even %d more than that)\n", totalCups - cups);
+            }
+
+            else {
+                System.out.printf("No, I can make only %d cup(s) of coffee\n", totalCups);
+            }
 
 
         } catch (IOException e) {
@@ -33,17 +38,73 @@ public class Coffee {
     }
 
 
+
+    private int totalCups(int water, int milk, int BEANS) {
+        int cupCount = 0;
+        int waterAmount = water;
+        int milkAmount = milk;
+        int beanAmount = BEANS;
+
+        while (true) {
+            if (waterAmount - 200 < 0) {
+                break;
+            } else {
+                waterAmount -= 200;
+            }
+
+            if (milkAmount - 50 < 0) {
+                break;
+            } else {
+                milkAmount -= 50;
+            }
+
+            if (beanAmount - 15 < 0) {
+                break;
+            } else {
+                beanAmount -= 15;
+            }
+
+            cupCount++;
+        }
+
+        return cupCount;
+    }
+
+
+
+
     //Constructors
     public Coffee() {}
 
-    public Coffee(int coffeeBeans, int water, int milk) {
-        this.coffeeBeans = coffeeBeans;
+    public Coffee(int water, int milk, int beans) {
+        this.coffeeBeans = beans;
         this.water = water;
         this.milk = milk;
     }
 
 
-    //Getters and Setters
+    //Getters, Setters, Askers.
+    public static int askWater() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write how many ml of water the coffee machine has:");
+        System.out.print("> ");
+        return sc.nextInt();
+    }
+
+    public static int askMilk() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        System.out.print("> ");
+        return sc.nextInt();
+    }
+
+    public static int askBeans() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        System.out.print("> ");
+        return sc.nextInt();
+    }
+
     public int getCoffeeBeans() {
         return coffeeBeans;
     }
--- coffeemachine/src/coffee_machine/Main.java
+++ coffeemachine/src/coffee_machine/Main.java
@@ -1,8 +1,8 @@
 package coffee_machine;
 
-public class Main {
+public class Main extends Coffee {
     public static void main(String[] args) {
-        Coffee coffee = new Coffee();
+        Coffee coffee = new Coffee(askWater(), askMilk(), askBeans());
         coffee.makeBeanJuice();
     }
 }
